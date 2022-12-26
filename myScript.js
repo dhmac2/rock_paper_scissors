e2acbbdd4abbf5a294198cb497463ad3d739cd07
dhmac2 <118634961+dhmac2@users.noreply.github.com>
2022-12-25 21:01:58 -0600

    Completed the game and score part of the project

--- myScript.js
+++ myScript.js
@@ -1,3 +1,8 @@
+let playerSelection;
+let computerSelection;
+let playerScore;
+let computerScore;
+
 function getComputerChoice () {
     value = Math.floor(Math.random()*3)+1;
     console.log(value)
@@ -19,13 +24,38 @@ function playRound(playerSelection, computerSelection) {
         return "You Tied! " + playerSelection + " ties " + computerSelection
     } else if ((playerSelection === "Rock" & computerSelection === "Scissor") || (playerSelection === "Paper" & computerSelection === "Rock")
     || (playerSelection === "Scissor" & computerSelection === "Paper") ){
-        return "You Won! " + playerSelection + " beats " + computerSelection
+        playerScore++;
+        return "You Won! " + playerSelection + " beats " + computerSelection;
     } else {
-        return "You Lost! " + computerSelection + " beats " + playerSelection
+        computerScore++;
+        return "You Lost! " + computerSelection + " beats " + playerSelection;
     }
 
 }
 
-const playerSelection =  window.prompt("What do you pick? Rock, Paper or Scissor?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function scoreChecker(playerScore, computerScore) {
+    if (playerScore >= 3) {
+        return "Congrats! You won with " + playerScore + " over " + computerScore;
+    } else if (computerScore >= 3) {
+        return "Better luck next time. The Computer won " + computerScore + " over " + playerScore;
+    }
+    else {
+        return "It's a draw. Try again"
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection =  window.prompt("What do you pick? Rock, Paper or Scissor?");
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    console.log(scoreChecker(playerScore, computerScore))
+}
+
+
+// const playerSelection =  window.prompt("What do you pick? Rock, Paper or Scissor?");
+// const computerSelection = getComputerChoice();
+
+game()
